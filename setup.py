6c5d0208d835ad195da2c1ae4e20c4638d050ed2
Matti Lamppu <lamppu.matti.akseli@gmail.com>
2021-11-08 10:57:24 +0200

    Update setup script. Bump version to 0.0.4.
    Setup script stopped working at some point but this should fix it.

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
-from setuptools import setup
+from distutils.core import setup
 from Cython.Build import cythonize
-
+from pathlib import Path
 
 setup(
-    ext_modules=cythonize("round2/round2.pyx"),
+    name="round2",
+    ext_modules=cythonize(
+        [
+            str(Path(__file__).parent / "round2" / "round2.pyx"),
+        ]
+    ),
     zip_safe=False,
 )
